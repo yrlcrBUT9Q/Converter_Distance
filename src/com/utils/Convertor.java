cb6ef1e5668e0124d1b52a5b64d722cea29e857a
kirill <pashkevich99333@gmail.com>
2025-03-09 23:24:38 +0200

    feat/add discriptionNew

--- src/com/utils/Convertor.java
+++ src/com/utils/Convertor.java
@@ -15,18 +15,22 @@ public class Convertor {
     public static double getConverterMlToKm(double ml) {
         return ml * CONVERTER_DISTANCE;
     }
-
-
+/*
+*In this function:
+*   1. write greetings
+    2. if you want find miles from kilometers, program use first "if", or if you want search kilometers from miles,
+    program will use second "if", then program stopped on 1 second, and all starts again.
+ */
     public static void bodyFunction(Scanner scanner, String choice) {
         System.out.println("Please write distance:");
         try {
             if (choice.equals(arrayString[0])) {
                 int km = Integer.parseInt(scanner.next());
-                System.out.println(km + " km = " + Convertor.getConverterKmToMl(km) + " miles \n");
+                System.out.println(km + arrayString[0] + " = " + Convertor.getConverterKmToMl(km) + arrayString[1] + " \n");
                 Thread.sleep(1000);
             } else if (choice.equals(arrayString[1])) {
                 int ml = Integer.parseInt(scanner.next());
-                System.out.println(ml + " ml = " + Convertor.getConverterMlToKm(ml) + " kilometres \n");
+                System.out.println(ml + arrayString[1] + " = " + Convertor.getConverterMlToKm(ml) + arrayString[0] + " \n");
                 Thread.sleep(1000);
             }
 
